ef5228cf6817d4c04bdf29f3f586fedeefd6cf9a
Kelly Johnson <kmshimko@gmail.com>
2016-01-07 23:39:13 -0600

    Changed file path.  Need still to correct links

---
+++ server/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('port', process.env.PORT || 3000);
+
+
+app.get('/*', function (request, response) {
+    var file = request.params[0] || '../public/views/index.html';
+
+    response.sendFile(path.join(__dirname, 'public', file));
+});
+
+
+app.listen(app.get('port'), function(){
+    console.log("I am listening on port: " + (app.get('port')));
+});
