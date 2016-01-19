7929239b120243ea42a50e483a04e39cf841186f
Kelly Johnson <kmshimko@gmail.com>
2016-01-19 10:19:46 -0600

    Placed files in root directory, files found

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/**
+ * Created by KellyShimkoJohnson on 1/18/16.
+ */
+
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+    // place code for your default task here
+});
--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ app.set('port', process.env.PORT || 3000);
 
 
 app.get('/*', function (request, response) {
-    var file = request.params[0] || '../public/views/index.html';
+    var file = request.params[0] || 'views/index.html';
 
     response.sendFile(path.join(__dirname, 'public', file));
 });
